5f20c2249f8e23d79facd3ee5e62a45e23cd4003
Brenton Cleeland <brenton@brntn.me>
2017-07-01 18:32:20 +1000

    🏃🏻 Keep the sync enabled!

--- runsync.py
+++ runsync.py
@@ -5,8 +5,6 @@ from providers.smashrun import SmashrunProvider
 if __name__ == '__main__':
     sp = StravaProvider()
     sr = SmashrunProvider()
-    # activities = sp.get_activities()
-    # for a in activities:
-    #     sr.add_activity(a)
-
-    sr.get_activities()
+    activities = sp.get_activities()
+    for a in activities:
+        sr.add_activity(a)
